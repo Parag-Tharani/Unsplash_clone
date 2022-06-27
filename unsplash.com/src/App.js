a8317b838fa23ad09bfd9df82557e771ec54bcee
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-06-27 10:10:58 +0530

    update

--- unsplash.com/src/App.js
+++ unsplash.com/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [items, setitem] = React.useState([]);
+  
+      React.useEffect(() => {
+        fetch(`http://localhost:8080/products`)
+          .then((res) => res.json())
+          .then((res) => setusers(res.data));
+      }, []);
   return (
-    <div className="App">
-      
+    <div>
+      <h1>users listing page</h1>
+      {items?.map((user) => (
+        <p key={user.id}>{user.id}</p>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
