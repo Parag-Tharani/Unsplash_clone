486836374f0d26754bc50cca020202ec1dd8d356
Parag Tharani <paragtharani24@gmail.com>
2022-06-27 22:46:02 +0530

    Init

--- unsplash_clone/src/App.js
+++ unsplash_clone/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import { SmoothScroll } from "scroller-motion"
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar />
     </div>
   );
 }
