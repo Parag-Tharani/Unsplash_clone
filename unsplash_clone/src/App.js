8d99f06fbb23a166ada0e2b1388e7ad24ec5a708
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-06-27 14:55:33 +0530

    Update App.js

--- unsplash_clone/src/App.js
+++ unsplash_clone/src/App.js
@@ -2,9 +2,26 @@
 import './App.css';
 
 function App() {
+  const [items, setitem] = React.useState([]);
+
+function fetchdata() {
+  fetch(`http://localhost:8080/products`)
+    .then((res) => res.json())
+    .then((res) => setitem(res.results));
+}
+  for (let index = 0; index < 3; index++) {
+    
+    
+  }
+  
   return (
     <div className="App">
-     
+      {items?.map((item) => (
+        <div key={item.id} className="card">
+          <h4>Product {item.id}</h4>
+          
+        </div>
+      ))}
     </div>
   );
 }
