768e0d81cf032833d5a2250298673d3f4f2143db
YASHV1752 <yashv1752@gmail.com>
2022-06-29 08:31:16 +0530

    Load More Functionality Added

--- unsplash.com/src/components/BlogHome.jsx
+++ unsplash.com/src/components/BlogHome.jsx
@@ -6,7 +6,7 @@ import { Blogcard2 } from './Blogcard2'
 import { BlogFooter } from './BlogFooter'
 
 export const BlogHome = () => {
-  const data = [
+  var data = [
     {
       id:1,
       url: "https://unsplash.com/blog/content/images/size/w600/2022/06/DeepMind--Blog-.jpg",
@@ -69,7 +69,7 @@ export const BlogHome = () => {
       type: "Community",
       title:"Meet Frankie Cordoba",
       by: "Natalie Brennan - ",
-      date: "MAy 11, 2022"
+      date: "May 11, 2022"
     },
     {
       id:9,
@@ -77,11 +77,65 @@ export const BlogHome = () => {
       type: "Announcement",
       title:"A look back on April",
       by: "Annie spratt - ",
-      date: "MAy 6, 2022"
+      date: "May 6, 2022"
+    },
+    {
+      id:10,
+      url: "https://unsplash.com/blog/content/images/size/w600/2022/05/Motherhood--Blog--2.jpg",
+      type: "Community",
+      title:"Motherhood & Photography",
+      by: "Alex Begin - ",
+      date: "May 5, 2022"
+    },
+    {
+      id:11,
+      url: "https://unsplash.com/blog/content/images/size/w600/2022/05/Spring--Blog-.jpg",
+      type: "Announcement",
+      title:"Fresh wapys to capture the Spring",
+      by: "Alex Begin - ",
+      date: "May 3, 2022"
+    },
+    {
+      id:12,
+      url: "https://unsplash.com/blog/content/images/size/w600/2022/04/Olly--Blog-.jpg",
+      type: "Announcement",
+      title:"Meet the Unsplash Team: Olly Ash",
+      by: "Natalie Brennan - ",
+      date: "May 2, 2022"
     }
   ]
-
-
+  const [data2, setdata] = React.useState(data);
+  var data1 = [
+    {
+      id:1,
+      url: "https://unsplash.com/blog/content/images/size/w600/2022/06/DeepMind--Blog-.jpg",
+      type: "Announcement",
+      title:"Unsplash ✕ DeepMind",
+      by: "Natalie Brennan - ",
+      date: "June 20, 2022"
+    },
+    {
+      id:2,
+      url: "https://unsplash.com/blog/content/images/size/w600/2022/05/Tim--Blog--2.jpg",
+      type: "Announcement",
+      title:"Meet the Unsplash Team",
+      by: "Natalie Brennan - ",
+      date: "June 7, 2022"
+    },
+    {
+      id:3,
+      url: "https://unsplash.com/blog/content/images/size/w600/2022/06/Amy-Shamblen--Twitter-.jpg",
+      type: "Community",
+      title:"Meet Amy Shamblen",
+      by: "Alex -Begin - ",
+      date: "June 9, 2022"
+    }
+  ]
+  const more = () => {
+    data = data.concat(data1);
+    setdata(data);
+    console.log(data);
+  }
 
   return (
     <div>
@@ -103,7 +157,7 @@ export const BlogHome = () => {
         </div>
         <div className='smallCard'>
             {
-              data.map((item)=>(
+              data2.map((item)=>(
                 <div key={item.id}>
                   <Blogcard data={item}/>
                 </div>
@@ -111,7 +165,7 @@ export const BlogHome = () => {
             }
         </div>
         <div style={{marginTop: "10rem",textAlign: "center"}}>
-          <button className='loadButton'>Load More Posts</button>
+          <button className='loadButton' onClick={more}>Load More Posts</button>
         </div>
         <div>
           <BlogFooter />
