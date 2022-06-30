d3ed0462a05b78bfc1c8a15bb03eba4e1e6a2f89
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-06-30 21:22:47 +0530

    update in components

--- unsplash_clone/src/App.js
+++ unsplash_clone/src/App.js
@@ -1,66 +1,201 @@
-import React from 'react';
-import './App.css';
+// import React from "react";
+import "./App.css";
+import banner from "./banner.png";
+import squarespace from "./squarespace.svg";
+import React, { useState, useEffect } from "react";
+import { Loader } from "./components/Loader";
+import InfiniteScroll from "react-infinite-scroll-component";
+import search from './components/search'
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
-  const [items, setitem] = React.useState([]);
-  const [items1, setitem1] = React.useState([]);
-  const [items2, setitem2] = React.useState([]);
- var index =1
-// function fetchdata(index) {
-  
-// }
-  for ( index; index < 2; index++) {
-    fetch(`https://api.unsplash.com/search/photos?page=${index}&query=office&per_page=30&client_id=DXYD_GyjDUwagXbKcj_4HvYGwkxDp8HKCkwXUJ12rT8`)
-      .then((res) => res.json())
-      .then((res) => setitem(res.results)); fetch(`https://api.unsplash.com/search/photos?page=${index+1}&query=office&per_page=30&client_id=DXYD_GyjDUwagXbKcj_4HvYGwkxDp8HKCkwXUJ12rT8`)
-        .then((res) => res.json())
-      .then((res) => setitem1(res.results)); fetch(`https://api.unsplash.com/search/photos?page=${index +2}&per_page=30&query=office&client_id=DXYD_GyjDUwagXbKcj_4HvYGwkxDp8HKCkwXUJ12rT8`)
-        .then((res) => res.json())
-        .then((res) => setitem2(res.results));
-  }
-  
-  return (
-    <> <>
-    <div className="App">
-      <div className="image_box">
-        {items?.map((item) => (
-                <div key={item.id} className="card">
-                  <img src={item.urls.full} alt="" />
-                </div>
-              ))}
-      </div>
-      <div className="image_box">
-          {items1?.map((item) => (
-        <div key={item.id} className="card">
-          <img src={item.urls.full} alt="" />
-        </div>
-      ))}
-      </div>
-      <div className="image_box">
-            {items2?.map((item) => (
-        <div key={item.id} className="card">
-          <img src={item.urls.small} alt="" />
-        </div>
-      ))}
-      </div>          
-      </div>
-    </>
-    </>
+  const [images, setImage] = useState([]);
+  const [search, setsearch] = useState([]);
 
+  const [images2, setimage2] = React.useState([]);
+  const [images3, setimage3] = React.useState([]);
+  const [index, setindex] = React.useState([1]);
+  // var index=1
+
+  useEffect(() => {
+    FetchImages();
+  }, []);
+
+  const FetchImages = ( ) => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
+
+  
+    fetch(
+      `https://api.unsplash.com/search/photos?page=${index }&per_page=30&query=office&client_id=DXYD_GyjDUwagXbKcj_4HvYGwkxDp8HKCkwXUJ12rT8`
+    )
+      .then((res) => res.json())
+      .then((res) => setImage(res.results));
+    fetch(
+      `https://api.unsplash.com/search/photos?page=${index +1}&per_page=30&query=office&client_id=DXYD_GyjDUwagXbKcj_4HvYGwkxDp8HKCkwXUJ12rT8`
+    )
+      .then((res) => res.json())
+      .then((res) => setimage2(res.results));
+    fetch(
+      `https://api.unsplash.com/search/photos?page=${index +2}&per_page=30&query=office&client_id=DXYD_GyjDUwagXbKcj_4HvYGwkxDp8HKCkwXUJ12rT8`
+    )
+      .then((res) => res.json())
+      .then((res) => setimage3(res.results));
+   
+  }; 
+  const display_search_demo = () => {
+    return <>
+
+    </>
+  }
+
+  return (
+    <>
+      <div className="top">
+        <div className="top_data">
+          <h1>Unsplash</h1>
+          <p>The internet’s source of freely-usable images. </p>
+          <p> Powered by creators everywhere.</p>
+
+          <div className="search_box">
+            <input
+              type="text"
+              placeholder="Search free high-resolution photos"
+              onClick={display_search_demo}
+            />
+          </div>
+          <div className="mousehover">
+            <p>Trending Searches</p>
+            <div className="trending_searches">
+              <p>Wallpapers</p>
+              <p>travel</p>
+              <p>home</p>
+              <p>stagelights</p>
+              <p>yoga</p>
+            </div>
+            <p>Trending Topics</p>
+            <div className="trending_topics">
+              <p>Wallpapers</p>
+              <p>Arts & Culture</p>
+              <p>3D Renders</p>
+              <p>Architecture</p>
+              <p>Textures & Patterns</p>
+            </div>
+            <p>Trending Collections</p>
+            <div className="trending_collections">
+              <p>Light Tones</p>
+              <p>Medium frames in interior</p>
+              <p>Springs</p>
+              <p>MockUps</p>
+              <p>Flat Lay Lifestyle</p>
+            </div>
+          </div>
+        </div>
+        <div className="banner">
+          <img src={banner} alt="" />
+        </div>
+        <div className="bottom">
+          <div className="first1">
+            <p>Photo by Stephan Leonardi</p>
+          </div>
+          <div className="second2">
+            <p>Read more about the Unsplash License</p>
+          </div>
+          <div className="third_3">
+            <div className="third3">
+              <img src={squarespace} alt="" />
+            </div>
+            <p>All you need to create a website</p>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="App">
+          {/* <div className="image_box"> */}
+          {/* {images?.map((item) => (
+              <div key={item.id} className="card">
+                <div className="top2">
+                  <p>+</p>
+                </div>
+                <img src={item.urls.regular} alt="" />
+                <a href={item.links.download_location}> Download Image</a>
+              </div>
+            ))} */}
+          {/* </div> */}
+          <div className="image_box">
+            <InfiniteScroll
+              dataLength={images.length}
+              next={FetchImages}
+              hasMore={true}
+              loader={<Loader />}
+            >
+              <>
+                {images?.map((image) => (
+                  <div key={image.id} className="card">
+                    {/* <div className="top4">
+                      <p>+</p>
+                    </div> */}
+                    <img src={image.urls.regular} alt="" />
+                  </div>
+                ))}
+              </>
+            </InfiniteScroll>
+          </div>
+          <div className="image_box">
+            <InfiniteScroll
+              dataLength={images.length}
+              next={FetchImages}
+              hasMore={true}
+              loader={<Loader />}
+            >
+              <>
+                {images2?.map((image) => (
+                  <div key={image.id} className="card">
+                    {/* <div className="top4">
+                      <p>+</p>
+                    </div> */}
+                    <img src={image.urls.regular} alt="" />
+                  </div>
+                ))}
+              </>
+            </InfiniteScroll>
+          </div>
+          <div className="image_box">
+            <InfiniteScroll
+              dataLength={images.length}
+              next={FetchImages}
+              hasMore={true}
+              loader={<Loader />}
+            >
+              <>
+                {images3?.map((image) => (
+                  <div key={image.id} className="card">
+                    {/* <div className="top4">
+                      <p>+</p>
+                    </div> */}
+                    <img src={image.urls.regular} alt="" />
+                  </div>
+                ))}
+              </>
+            </InfiniteScroll>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
 
-
-
-
-// import { useState } from "react";
+{
+  /* // import { useState } from "react";
 
 // import { Container, Grid,  Item, Button } from "semantic-ui-react";
 // import "./App.css";
 
-// function App() {
+// function App() { */
+}
 //   const [origImage, setOrigImage] = useState("");
 //   const [origImageFile, setOrigImageFile] = useState("");
 //   const [fileName, setFileName] = useState("");
@@ -71,8 +206,8 @@ export default App;
 //     setOrigImage(imageFile);
 //     setOrigImageFile(URL.createObjectURL(imageFile));
 //     setFileName(imageFile.name);
-//   }; 
-   
+//   };
+
 //   return (
 //     <div className="App">
 //       <Container>
@@ -101,6 +236,29 @@ export default App;
 //     </div>
 //   );
 
+// }
+
+// export default App;
+
+// function App() {
+
+//   return (
+//     <div>
+//       <Heading />
+//       <InfiniteScroll
+//         dataLength={images.length}
+//         next={fetchImages}
+//         hasMore={true}
+//         loader={<Loader />}
+//       >
+//         <WrapperImages>
+//           {images.map(image => (
+//             <UnsplashImage url={image.urls.thumb} key={image.id} />
+//           ))}
+//         </WrapperImages>
+//       </InfiniteScroll>
+//     </div>
+//   );
 // }
 
 // export default App;
